a6e7a6f8149acb6e74d880e981d933ce812619fa
Safyan Akram <msafyan080@gmail.com>
2020-05-30 17:55:25 +0500

    changed

--- app/containers/App/index.js
+++ app/containers/App/index.js
@@ -37,7 +37,7 @@ export default function App() {
       >
         <meta name="description" content="A React.js Boilerplate application" />
       </Helmet>
-      <Header />
+      {/* <Header /> */}
       <Switch>
         <Route exact path="/" component={HomePage} />
         <Route path="/features" component={FeaturePage} />
